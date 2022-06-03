0030d2d680a63ee3b27f8d0997ca3a2d11a40a82
yockgen <yockgen@gmail.com>
2022-06-03 22:44:17 +0800

    Update listener.py

--- basic/listener.py
+++ basic/listener.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python           # This is server.py file
-
+import sys
 import socket               # Import socket module
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)         # Create a socket object
-host = '192.168.0.9' #socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+host = sys.argv[1]          #'173.16.1.200'       #socket.gethostname() # Get local machine name
+port = int(sys.argv[2])          #12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
+print ("host ",host)
+print ("port ", port)
 
 s.listen(5)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
    print ('Got connection from ', addr)
+   talkerMsg  = c.recv(1024).decode()
+   print('receiving:' ,talkerMsg )
 
-   talkerMsg  = "dummy" #c.recv(1024).decode()
-
-   print('msg from talker - ', talkerMsg )
-    
-   strMsg = 'yockgen!!!!!yockgen!!!' +  talkerMsg
+   strMsg = 'server response:' +  talkerMsg
    c.send(strMsg.encode())
    c.close()                # Close the connection
+
+#python3  server.py 173.16.1.200 12345
